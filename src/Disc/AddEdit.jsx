93d3a5302f38bb7c1397e575a499963cde3e05e6
yerayherreria <yerayherre@gmail.com>
2024-01-18 12:34:30 +0100

    edit disc

---
+++ src/Disc/AddEdit.jsx
@@ -0,0 +1,110 @@
+import { useEffect, useState } from 'react'
+import { createDisc, getDisc, updateDisc } from '../services/DiscServices';
+
+function Add({id}) {
+    //6564598af353f12c24ad022b
+    
+    const [disc, setDisc] = useState({ name: "", numSong: 0, category: "", duration: 0 });
+
+    function handleData(e){
+        setDisc({...disc,[e.target.name]:e.target.value});
+    }   
+
+    function addSave(){
+        createDisc(disc).then(() => {
+          });
+    }
+    function editSave(){
+        console.log(disc);
+        updateDisc(id,disc).then(() => {
+        });
+    }
+    if(id!=null){
+
+    function viewData(idData){
+        useEffect(() => {
+            getDisc(idData).then((response) => {
+                setDisc(response.data);
+                console.log(disc);
+            });
+          }, []);
+    }
+        viewData(id);
+    }
+    function form(){
+        if(id!=null){
+            return (
+                <>          <h1>Edit</h1>
+                            <div>
+                                <label htmlFor="name">
+                                    name:<br />
+                                    <input type="text" name="name" onChange={handleData} value={disc.name}/>
+                                </label>
+                            </div>
+                            <div>
+                                <label htmlFor="numSong">
+                                numSong:<br />
+                                    <input type="number" name="numSong" onChange={handleData} value={disc.numSong} />
+                                </label>
+                            </div>
+                            <div>
+                                <label htmlFor="category">
+                                category:<br />
+                                    <input type="text" name="category" onChange={handleData} value={disc.category} />
+                                </label>
+                            </div>
+                            <div>
+                                <label htmlFor="duration">
+                                duration:<br />
+                                    <input type="number" name="duration" onChange={handleData} value={disc.duration} />
+                                </label>
+                            </div>
+                            <br />
+                            <div>
+                                <button type="submit" onClick={editSave}>Edit</button>
+                            </div>
+                           
+                </>
+            )
+        } else {
+            return (
+                <>          <h1>Add</h1>
+                            <div>
+                                <label htmlFor="name">
+                                    name:<br />
+                                    <input type="text" name="name" onChange={handleData} />
+                                </label>
+                            </div>
+                            <div>
+                                <label htmlFor="numSong">
+                                numSong:<br />
+                                    <input type="number" name="numSong" onChange={handleData} />
+                                </label>
+                            </div>
+                            <div>
+                                <label htmlFor="category">
+                                category:<br />
+                                    <input type="text" name="category" onChange={handleData} />
+                                </label>
+                            </div>
+                            <div>
+                                <label htmlFor="duration">
+                                duration:<br />
+                                    <input type="number" name="duration" onChange={handleData} />
+                                </label>
+                            </div>
+                            <br />
+                            <div>
+                                <button type="submit" onClick={addSave}>Save</button>
+                            </div>
+                           
+                </>
+            )
+        }
+    }
+  return (
+    form()
+  )
+}
+
+export default Add;
--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,12 @@ import App from './App.jsx'
 import './index.css'
 import Discs from './Disc/ListDisc.jsx'
 import Disc from './Disc/ViewDisc.jsx'
-import Add from './Disc/Add.jsx'
+import Add from './Disc/AddEdit.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/*<Disc />*/}
     <Discs />
-    <Add />
+    <Add id="6564598af353f12c24ad022b"/>
   </React.StrictMode>,
 )
