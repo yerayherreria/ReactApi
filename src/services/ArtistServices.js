5cbde0dfa51376005bbda012ecc9057cb0a9e949
yerayherreria <yerayherre@gmail.com>
2024-01-29 08:47:04 +0100

    .

--- src/services/ArtistServices.js
+++ src/services/ArtistServices.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = 'http://localhost:3000/artist';
+const baseURL = 'https://project-three-models.vercel.app/artist';
 
 const apiService = axios.create({
 baseURL,
--- src/services/DiscServices.js
+++ src/services/DiscServices.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = 'http://localhost:3000/disc';
+const baseURL = 'https://project-three-models.vercel.app/disc';
 
 const apiService = axios.create({
 baseURL,
